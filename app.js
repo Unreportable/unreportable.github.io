6cd9cace37a0ac6148b5c78dfaa0e3706bde9cf2
Matthew Golikov <unreportable@gmail.com>
2017-12-10 00:04:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,39 +1,95 @@
-var lock = new Auth0Lock('YOUR_CLIENT_ID', 'YOUR_DOMAIN');
+window.addEventListener('load', function() {
+  var content = document.querySelector('.content');
+  var loadingSpinner = document.getElementById('loading');
+  content.style.display = 'block';
+  loadingSpinner.style.display = 'none';
 
+  var webAuth = new auth0.WebAuth({
+    domain: AUTH0_DOMAIN,
+    clientID: AUTH0_CLIENT_ID,
+    redirectUri: AUTH0_CALLBACK_URL,
+    audience: 'https://' + AUTH0_DOMAIN + '/userinfo',
+    responseType: 'token id_token',
+    scope: 'openid',
+    leeway: 60
+  });
 
+  var loginStatus = document.querySelector('.container h4');
+  var loginView = document.getElementById('login-view');
+  var homeView = document.getElementById('home-view');
 
-document.getElementById('btn-login').addEventListener('click', function() {
-    lock.show(function(err, profile, token) {
-      if (err) {
-        // Error callback
-        console.error("Something went wrong: ", err);
-      } else {
-        // Success calback  
-  
-        // Save the JWT token.
-        localStorage.setItem('userToken', token);
-        // Save the profile
-        localStorage.setItem('userProfile', JSON.stringify(profile));
+  // buttons and event listeners
+  var homeViewBtn = document.getElementById('btn-home-view');
+  var loginBtn = document.getElementById('btn-login');
+  var logoutBtn = document.getElementById('btn-logout');
+
+  homeViewBtn.addEventListener('click', function() {
+    homeView.style.display = 'inline-block';
+    loginView.style.display = 'none';
+  });
+
+  loginBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    webAuth.authorize();
+  });
+
+  logoutBtn.addEventListener('click', logout);
+
+  function setSession(authResult) {
+    // Set the time that the access token will expire at
+    var expiresAt = JSON.stringify(
+      authResult.expiresIn * 1000 + new Date().getTime()
+    );
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
+  }
+
+  function logout() {
+    // Remove tokens and expiry time from localStorage
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+    displayButtons();
+  }
+
+  function isAuthenticated() {
+    // Check whether the current time is past the
+    // access token's expiry time
+    var expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
+  }
+
+  function handleAuthentication() {
+    webAuth.parseHash(function(err, authResult) {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        window.location.hash = '';
+        setSession(authResult);
+        loginBtn.style.display = 'none';
+        homeView.style.display = 'inline-block';
+      } else if (err) {
+        homeView.style.display = 'inline-block';
+        console.log(err);
+        alert(
+          'Error: ' + err.error + '. Check the console for further details.'
+        );
       }
+      displayButtons();
     });
-  });
+  }
 
+  function displayButtons() {
+    if (isAuthenticated()) {
+      loginBtn.style.display = 'none';
+      logoutBtn.style.display = 'inline-block';
+      loginStatus.innerHTML = 'You are logged in!';
+    } else {
+      loginBtn.style.display = 'inline-block';
+      logoutBtn.style.display = 'none';
+      loginStatus.innerHTML =
+        'You are not logged in! Please log in to continue.';
+    }
+  }
 
-
-  var getFoos = fetch('/api/foo', {
-    headers: {
-      'Authorization': 'Bearer ' + localStorage.getItem('userToken')
-    },
-    method: 'GET',
-    cache: false
-  });
-  
-  getFoos.then(function (response) {
-    response.json().then(function (foos) {
-      console.log('the foos:', foos);
-    });
-  });
-
- // localStorage.removeItem('userToken');
-//  localStorage.removeItem('userProfile');
- // window.location.href = "/";
+  handleAuthentication();
+});
