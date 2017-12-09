65b6faf45d84e10ea2c9baf68ead4e4a5362d09e
Matthew Golikov <unreportable@gmail.com>
2017-12-10 00:20:24 +0300

    Update auth0-variables.js

--- auth0-variables.js
+++ auth0-variables.js
@@ -1,3 +1,3 @@
-var AUTH0_CLIENT_ID='8da19E1AAmL9WnY-d1vH2cd10g43X8NV';
+var AUTH0_CLIENT_ID='JDaQgVp35PQVIHyg5hR0Rgf0D7Bx7172';
 var AUTH0_DOMAIN='unreportable.eu.auth0.com';
-var AUTH0_CALLBACK_URL=location.href;
+var AUTH0_CALLBACK_URL='https://unreportable.github.io/';
