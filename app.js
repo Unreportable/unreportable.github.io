9473a70fcbcfb1e5f491f68e7a6f5ffa94141532
Matthew Golikov <unreportable@gmail.com>
2017-12-11 07:25:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -175,9 +175,9 @@ buttSendTELEGRAM.addEventListener('click',function(e){
 firebase.database().ref("orders/").on("value", function(snapshot) { 
 snapshot.forEach(function(childSnapshot) { 
 var key = childSnapshot.key; 
-var childData = childSnapshot.val(); ; 
+var childData = childSnapshot.val();  
 numzak = childData;
-
+console.log(numzak+"ya tyta");
 }); 
 }); 
  } 
